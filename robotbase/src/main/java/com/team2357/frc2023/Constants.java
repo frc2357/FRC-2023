11987b5950f807d128cd59d54821261c3043f867
Tyson453 <95186148+Tyson453@users.noreply.github.com>
2023-01-12 20:39:02 -0800

    Finish changing rotation PID to ProfiledPIDController

--- robotbase/src/main/java/com/team2357/frc2023/Constants.java
+++ robotbase/src/main/java/com/team2357/frc2023/Constants.java
@@ -65,12 +65,14 @@ public final class Constants {
             config.m_maxVelocityMetersPerSecond = 6380.0 / 60.0 *
                     SdsModuleConfigurations.MK4I_L2.getDriveReduction() *
                     SdsModuleConfigurations.MK4I_L2.getWheelDiameter() * Math.PI;
+
             config.m_maxAngularVelocityRadiansPerSecond = config.m_maxVelocityMetersPerSecond /
                     Math.hypot(config.m_trackwidthMeters / 2.0, config.m_wheelbaseMeters / 2.0);
+            config.m_maxAngularAccelerationRadiansPerSecondSquared = config.m_maxAngularVelocityRadiansPerSecond / 3.0;
 
             config.m_xController = new PIDController(.56122, 0, 0);
             config.m_yController = new PIDController(.56122, 0, 0);
-            config.m_thetaController = new ProfiledPIDController(2.15, 0, 0, new TrapezoidProfile.Constraints(config.m_maxAngularVelocityRadiansPerSecond, null));
+            config.m_thetaController = new ProfiledPIDController(2.15, 0, 0, new TrapezoidProfile.Constraints(config.m_maxAngularVelocityRadiansPerSecond, config.m_maxAngularAccelerationRadiansPerSecondSquared));
 
             return config;
         }
