0a42fec4a70b9f88c2b78cb71ef39ab71d636907
Tyson453 <95186148+Tyson453@users.noreply.github.com>
2023-01-22 13:13:56 -0800

    Add controls to run and reverse the intake

--- robotbase/src/main/java/com/team2357/frc2023/controls/SwerveDriveControls.java
+++ robotbase/src/main/java/com/team2357/frc2023/controls/SwerveDriveControls.java
@@ -1,5 +1,7 @@
 package com.team2357.frc2023.controls;
 
+import com.team2357.frc2023.commands.ReverseIntakeCommand;
+import com.team2357.frc2023.commands.RunIntakeCommand;
 import com.team2357.frc2023.subsystems.SwerveDriveSubsystem;
 import com.team2357.lib.util.XboxRaw;
 
@@ -12,14 +14,21 @@ public class SwerveDriveControls {
     private double m_deadband;
 
     private JoystickButton m_backButton;
+    private JoystickButton m_leftTrigger;
+    private JoystickButton m_leftBumper;
 
     public SwerveDriveControls(XboxController controller, double deadband) {
         m_controller = controller;
         m_deadband = deadband;
 
         m_backButton = new JoystickButton(m_controller, XboxRaw.Back.value);
+        m_leftTrigger = new JoystickButton(m_controller, XboxRaw.TriggerLeft.value);
+        m_leftBumper = new JoystickButton(m_controller, XboxRaw.BumperLeft.value);
 
         m_backButton.onTrue(new InstantCommand(() -> SwerveDriveSubsystem.getInstance().zeroGyroscope()));
+
+        m_leftTrigger.onTrue(new RunIntakeCommand());
+        m_leftBumper.onTrue(new ReverseIntakeCommand());
     }
 
     public double getX() {
