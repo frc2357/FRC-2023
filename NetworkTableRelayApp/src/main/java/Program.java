cb1b0b28d8e8446b7b44a090a242f42804d87e07
NolanCampbell2357 <nolanc1223@gmail.com>
2023-01-28 14:07:30 -0600

    Add test

--- NetworkTableRelayApp/src/main/java/Program.java
+++ NetworkTableRelayApp/src/main/java/Program.java
@@ -1,4 +1,8 @@
 
+import edu.wpi.first.networktables.DoublePublisher;
+import edu.wpi.first.networktables.DoubleSubscriber;
+import edu.wpi.first.networktables.DoubleTopic;
+import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.networktables.NetworkTablesJNI;
 import edu.wpi.first.util.CombinedRuntimeLoader;
@@ -22,5 +26,40 @@ public class Program {
         CombinedRuntimeLoader.loadLibraries(Program.class, "wpiutiljni", "wpimathjni", "ntcorejni", "cscorejnicvstatic");
 
         var inst = NetworkTableInstance.getDefault();
+        new Program().run();
+    }
+
+    public void run() {
+        NetworkTableInstance inst = NetworkTableInstance.getDefault();
+        NetworkTable table = inst.getTable("datatable");
+        //DoublePublisher testPub = table.getDoubleTopic("testTopic").publish();
+        DoubleSubscriber testSub = table.getDoubleTopic("testTopic").subscribe(0.0);
+        
+
+        // For local only?????
+        //inst.startClient4("example client");
+        //inst.setServer("localhost"); // where TEAM=190, 294, etc, or use inst.setServer("hostname") or similar
+        //inst.startDSClient(); // recommended if running on DS computer; this gets the robot IP from the DS
+       
+       double test = 0;
+        while (true) {
+            // try {
+            //     Thread.sleep(500);
+            // } catch (InterruptedException ex) {
+            //     System.out.println("interrupted");
+            //     return;
+            // }
+
+            // System.out.println("Test at start of iteration: "+test);
+            // double startTime = System.currentTimeMillis();
+            // testPub.set(test);
+
+            double x = testSub.get();
+            double timeStamp = testSub.getLastChange();
+            double difference = NetworkTablesJNI.now() - timeStamp;
+
+            System.out.println("test is: " + x + ", set and received in "+difference);
+            test+=0.5;
+        }
     }
 }
