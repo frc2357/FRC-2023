711f37a13c2da9c733e056e5b3d624f1ae0e6360
NolanCampbell2357 <nolanc1223@gmail.com>
2023-01-28 15:52:40 -0600

    Boolean array test

--- NetworkTableRelayApp/src/main/java/Program.java
+++ NetworkTableRelayApp/src/main/java/Program.java
@@ -1,4 +1,5 @@
 
+import edu.wpi.first.networktables.BooleanArraySubscriber;
 import edu.wpi.first.networktables.DoublePublisher;
 import edu.wpi.first.networktables.DoubleSubscriber;
 import edu.wpi.first.networktables.DoubleTopic;
@@ -8,6 +9,7 @@ import edu.wpi.first.networktables.NetworkTablesJNI;
 import edu.wpi.first.util.CombinedRuntimeLoader;
 
 import java.io.IOException;
+import java.util.Arrays;
 
 import edu.wpi.first.cscore.CameraServerJNI;
 import edu.wpi.first.math.WPIMathJNI;
@@ -24,8 +26,6 @@ public class Program {
         CameraServerJNI.Helper.setExtractOnStaticLoad(false);
 
         CombinedRuntimeLoader.loadLibraries(Program.class, "wpiutiljni", "wpimathjni", "ntcorejni", "cscorejnicvstatic");
-
-        var inst = NetworkTableInstance.getDefault();
         new Program().run();
     }
 
@@ -33,33 +33,30 @@ public class Program {
         NetworkTableInstance inst = NetworkTableInstance.getDefault();
         NetworkTable table = inst.getTable("datatable");
         //DoublePublisher testPub = table.getDoubleTopic("testTopic").publish();
-        DoubleSubscriber testSub = table.getDoubleTopic("testTopic").subscribe(0.0);
+        BooleanArraySubscriber testSub = table.getBooleanArrayTopic("testTopic").subscribe(new boolean[] {});
         
-
-        // For local only?????
-        //inst.startClient4("example client");
+        inst.startClient4("example client");
         //inst.setServer("localhost"); // where TEAM=190, 294, etc, or use inst.setServer("hostname") or similar
-        //inst.startDSClient(); // recommended if running on DS computer; this gets the robot IP from the DS
-       
-       double test = 0;
+        inst.setServerTeam(2357);
+        inst.startDSClient(); // recommended if running on DS computer; this gets the robot IP from the DS
+
         while (true) {
-            // try {
-            //     Thread.sleep(500);
-            // } catch (InterruptedException ex) {
-            //     System.out.println("interrupted");
-            //     return;
-            // }
+            try {
+                Thread.sleep(1);
+            } catch (InterruptedException ex) {
+                System.out.println("interrupted");
+                return;
+            }
 
             // System.out.println("Test at start of iteration: "+test);
             // double startTime = System.currentTimeMillis();
             // testPub.set(test);
 
-            double x = testSub.get();
+            boolean[] x = testSub.get();
             double timeStamp = testSub.getLastChange();
             double difference = NetworkTablesJNI.now() - timeStamp;
 
-            System.out.println("test is: " + x + ", set and received in "+difference);
-            test+=0.5;
+            System.out.println("test is: " + Arrays.toString(x) + ", set and received in "+difference);
         }
     }
 }
