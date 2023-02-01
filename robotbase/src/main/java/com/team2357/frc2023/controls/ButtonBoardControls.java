0163fc849d69319bec9924c22c39153c7497706a
Tyson453 <95186148+Tyson453@users.noreply.github.com>
2023-01-31 16:39:14 -0800

    Change to using only one trigger

--- robotbase/src/main/java/com/team2357/frc2023/controls/ButtonBoardControls.java
+++ robotbase/src/main/java/com/team2357/frc2023/controls/ButtonBoardControls.java
@@ -1,18 +1,15 @@
 package com.team2357.frc2023.controls;
 
 import com.team2357.frc2023.Constants;
-import com.team2357.lib.util.XboxRaw;
 
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
-import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 
 public class ButtonBoardControls {
     XboxController m_controller;
 
-    private Trigger m_rightJoystickX;
-    private Trigger m_rightJoystickY;
+    private Trigger m_rightJoystick;
 
     private int m_xValue, m_yValue;
 
@@ -21,10 +18,14 @@ public class ButtonBoardControls {
     public ButtonBoardControls(XboxController controller) {
         m_controller = controller;
 
-        m_rightJoystickX = new Trigger(() -> {return 1 >= m_controller.getRightX() && m_controller.getRightX() >= -1;});
-        m_rightJoystickY = new Trigger(() -> {return 1 >= m_controller.getRightY() && m_controller.getRightY() >= -1;});
+        m_rightJoystick = new Trigger(() -> {
+            return (1 >= m_controller.getRightX() && m_controller.getRightX() >= -1) &&
+                    (1 >= m_controller.getRightY() && m_controller.getRightY() >= -1);
+        });
 
-        m_rightJoystickX.onTrue(new InstantCommand(() -> {updateKeys();}));
+        m_rightJoystick.onTrue(new InstantCommand(() -> {
+            updateKeys();
+        }));
     }
 
     private void updateKeys() {
@@ -38,7 +39,7 @@ public class ButtonBoardControls {
     private int analogToIndex(double value) {
         value /= Constants.CONTROLLER.BUTTON_BOARD_JOYSTICK_MAX_VALUE;
         value *= 10;
-        return (int)value;
+        return (int) value;
     }
 
     private void clearKeys() {
