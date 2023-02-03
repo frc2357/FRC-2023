a81aef1f80a4c3dd97b798898044b0e63b0542c9
MidgleyM <115593302+MidgleyM@users.noreply.github.com>
2023-02-02 19:36:46 -0600

    started to add the command

---
+++ robotbase/src/main/java/com/team2357/frc2023/commands/PanicModeArmExtender.java
@@ -0,0 +1,11 @@
+package com.team2357.frc2023.commands;
+
+import com.team2357.frc2023.subsystems.ArmExtendSubsystem;
+
+import edu.wpi.first.wpilibj.AddressableLED;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+public class PanicModeArmExtender extends CommandBase{
+    public PanicModeArmExtender(){
+    }
+}
