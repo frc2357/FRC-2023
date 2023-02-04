bbfa0f0bcced066b6df1be44adeec13f96c8899d
MidgleyM <115593302+MidgleyM@users.noreply.github.com>
2023-02-04 11:21:16 -0600

    added manual wrist control

--- robotbase/src/main/java/com/team2357/frc2023/commands/PanicModeArmExtender.java
+++ robotbase/src/main/java/com/team2357/frc2023/commands/PanicModeArmExtender.java
@@ -1,7 +1,7 @@
 package com.team2357.frc2023.commands;
 
 import com.team2357.frc2023.subsystems.ArmExtendSubsystem;
-
+import com.team2357.frc2023.subsystems.WristSubsystem;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 
@@ -9,11 +9,20 @@ public class PanicModeArmExtender extends CommandBase{
     XboxController controller;
     public PanicModeArmExtender(XboxController m_controller){
         addRequirements(ArmExtendSubsystem.getInstance());
+        addRequirements(WristSubsystem.getInstance());
         controller = m_controller;
     }
     @Override 
     public void execute(){
         ArmExtendSubsystem.getInstance().extend(controller.getRightY());
+        if(controller.getAButtonPressed()){
+            if(WristSubsystem.getInstance().isExtended()){
+                WristSubsystem.getInstance().contract();
+            }
+            else{
+                WristSubsystem.getInstance().extend();
+            }
+        }
     }
     @Override
     public void end(boolean interrupted){
