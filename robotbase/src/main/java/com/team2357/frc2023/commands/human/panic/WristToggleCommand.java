d085f1067c78743ad4affa58e58eb2f441175dec
MidgleyM <115593302+MidgleyM@users.noreply.github.com>
2023-02-04 13:01:33 -0600

    Panic mode arm extension (#82)
    * started to add the command
    
    * created panic mode command for the arm extender
    
    * added manual wrist control
    
    * fixed potential issue with wrist control
    
    * fixed Issues with commands
    
    * fixed potential issue with wrist command
    
    * formatting corrections / making it look nicer
    
    * fixed issues, organized commands
    
    * fixed naming issues
    
    * fixed binding placement issue

---
+++ robotbase/src/main/java/com/team2357/frc2023/commands/human/panic/WristToggleCommand.java
@@ -0,0 +1,20 @@
+package com.team2357.frc2023.commands.human.panic;
+
+import com.team2357.frc2023.subsystems.WristSubsystem;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+public class WristToggleCommand extends CommandBase {
+    public WristToggleCommand() {
+        addRequirements(WristSubsystem.getInstance());
+    }
+
+    @Override
+    public void initialize() {
+        if (WristSubsystem.getInstance().isExtended() || WristSubsystem.getInstance().isExtending()) {
+            WristSubsystem.getInstance().contract();
+        } else {
+            WristSubsystem.getInstance().extend();
+        }
+    }
+}
