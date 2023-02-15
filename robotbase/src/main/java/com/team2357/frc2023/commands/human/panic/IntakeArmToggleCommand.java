d7229195594f5086054bee1ba626e91effdb1aa0
MidgleyM <115593302+MidgleyM@users.noreply.github.com>
2023-02-14 19:03:44 -0600

    test successful!

--- robotbase/src/main/java/com/team2357/frc2023/commands/human/panic/IntakeArmToggleCommand.java
+++ robotbase/src/main/java/com/team2357/frc2023/commands/human/panic/IntakeArmToggleCommand.java
@@ -17,7 +17,6 @@ public class IntakeArmToggleCommand extends CommandBase{
             m_arm.stow();
         }else{
             m_arm.deploy();
-            ]\]dfcx gb[ bopQWJAIOVNVCKL'
         }
     }
     @Override
