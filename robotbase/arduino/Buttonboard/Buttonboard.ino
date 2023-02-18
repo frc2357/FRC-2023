2e55ce906edfc877db47e43a9d99ed0e81ad7f59
MidgleyM <115593302+MidgleyM@users.noreply.github.com>
2023-02-18 11:48:47 -0600

    changed it so it got both values as Json objects

--- robotbase/arduino/Buttonboard/Buttonboard.ino
+++ robotbase/arduino/Buttonboard/Buttonboard.ino
@@ -4,39 +4,58 @@
 #define LOOP_DELAY_MS = 20
 const byte ROWS = 3; // rows
 const byte COLS = 9; // columns
-//define the symbols on the buttons of the keypads
+// define the symbols on the buttons of the keypads
 char keys[ROWS][COLS] = {
-  {'1','2','3','4','5','6','7','8','9'},
-  {'0','A','B','C','D','E','F','G','H'},
-  {'I','J','K','L','M','N','O','P','Q'},
+    {'1', '2', '3', '4', '5', '6', '7', '8', '9'},
+    {'0', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'},
+    {'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q'},
 };
-byte rowPins[ROWS] = {0, 10, 20}; //connect to the row pinouts of the keypad
-byte colPins[COLS] = {9, 1, 2, 3,4,5,6,7,8}; //connect to the column pinouts of the keypad
-//initialize an instance of class NewKeypad
-Adafruit_Keypad customKeypad = Adafruit_Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS);
+byte rowPins[ROWS] = {0, 10, 20};                 // connect to the row pinouts of the keypad
+byte colPins[COLS] = {9, 1, 2, 3, 4, 5, 6, 7, 8}; // connect to the column pinouts of the keypad
+// initialize an instance of class NewKeypad
+Adafruit_Keypad customKeypad = Adafruit_Keypad(makeKeymap(keys), rowPins, colPins, ROWS, COLS);
 
 SensorDevice_Seeed_XIAO_RP2040<1> device("ButtonBoard");
-//makes Json variables for what we need, returns 1 if a button was pressed, 0 if not.
-int readIntValue(int min, int max){  
-  customKeypad.tick();
-  while(customKeypad.available()){
+
+// makes Json variables for what we need, returns 1 if a button was pressed, 0 if not.
+int readRowValue(int min, int max)
+{
+  while (customKeypad.available())
+  {
     keypadEvent e = customKeypad.read();
-    if(e.bit.EVENT == KEY_JUST_PRESSED){
-      Json::Int("keyRow",e.bit.ROW);
-      Json::Int("keyCol",e.bit.COL);
-      return 1;
+    if (e.bit.EVENT == KEY_JUST_PRESSED)
+    {
+      return e.bit.ROW;
     }
-    return 0;
+    return -1;
   }
-  return 0;
+  return -1;
 }
-void setup() {
+
+int readColValue(int min, int max)
+{
+  while (customKeypad.available())
+  {
+    keypadEvent e = customKeypad.read();
+    if (e.bit.EVENT == KEY_JUST_PRESSED)
+    {
+      return e.bit.COL;
+    }
+    return -1;
+  }
+  return -1;
+}
+void setup()
+{
   customKeypad.begin();
-  device.initSensor("intValue",readIntValue,0,8);
+  device.initSensor("rowValue", readRowValue, 0, 2);
+  device.initSensor("columnValue", readColValue, 0, 8);
   device.begin();
 }
 
-void loop() {
+void loop()
+{
+  customKeypad.tick();
   device.update();
   delay(LOOP_DELAY_MS);
 }
