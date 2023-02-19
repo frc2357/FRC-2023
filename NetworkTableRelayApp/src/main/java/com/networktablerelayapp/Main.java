4d8e384ce9c85e663b46849f842ad5af3a2af72d
NolanCampbell2357 <nolanc1223@gmail.com>
2023-02-18 18:57:57 -0600

    Add row and col info

--- NetworkTableRelayApp/src/main/java/com/networktablerelayapp/Main.java
+++ NetworkTableRelayApp/src/main/java/com/networktablerelayapp/Main.java
@@ -6,6 +6,7 @@ import java.util.Arrays;
 import edu.wpi.first.cscore.CameraServerJNI;
 import edu.wpi.first.math.WPIMathJNI;
 import edu.wpi.first.networktables.BooleanArraySubscriber;
+import edu.wpi.first.networktables.IntegerPublisher;
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.networktables.NetworkTablesJNI;
@@ -19,6 +20,9 @@ public class Main {
 
     private static final String BUTTONBOARD_TABLE_NAME = "buttonboard";
     private static final String GRID_STATES_TOPIC_NAME = "GridStates";
+
+    private static final String ROW_TOPIC_NAME = "row";
+    private static final String COL_TOPIC_NAME = "col";
 
     private static final String NT4_CLIENT_IDENTITY = "network table relay";
     private static final int TEAM_NAME = 2357;
@@ -39,10 +43,15 @@ public class Main {
         BooleanArraySubscriber gridStatesSub = buttonboardTable.getBooleanArrayTopic(GRID_STATES_TOPIC_NAME).subscribe(new boolean[]{});
         boolean[] gridStates = new boolean[] {};
 
+        IntegerPublisher rowPub = buttonboardTable.getIntegerTopic(ROW_TOPIC_NAME).publish();
+        IntegerPublisher colPub = buttonboardTable.getIntegerTopic(COL_TOPIC_NAME).publish();
+
         inst.startClient4(NT4_CLIENT_IDENTITY);
         inst.setServerTeam(TEAM_NAME);
         inst.startDSClient(); 
 
+        int row = 0;
+        int col = 0;
         while (true) {
             boolean[] newGridStates = gridStatesSub.get();
 
@@ -51,6 +60,23 @@ public class Main {
 
                 // Send to ardunio
             }
+
+            rowPub.set(row);
+            colPub.set(col);
+            
+            row++;
+            if(row > 2) {
+                row = 0;
+            }
+
+            col++;
+            if(col > 8) {
+                col = 0;
+            }
+
+            try {
+            Thread.sleep(100);
+            } catch (InterruptedException e) {}
         }
     }
 }
