71e6c460bda98d4256c78c73898e803e0b72710b
Tyson453 <95186148+Tyson453@users.noreply.github.com>
2023-02-21 16:42:53 -0800

    Add todo

--- robotbase/src/main/java/com/team2357/frc2023/commands/scoring/teleopAutoScore/TeleopAutoScoreCommandGroup.java
+++ robotbase/src/main/java/com/team2357/frc2023/commands/scoring/teleopAutoScore/TeleopAutoScoreCommandGroup.java
@@ -10,6 +10,7 @@ import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 public class TeleopAutoScoreCommandGroup extends SequentialCommandGroup {
     public TeleopAutoScoreCommandGroup() {
         addCommands(new WaitForTargetCommand());
+        // TODO: Find a way to select an april tag to base this off of
         addCommands(new TranslateToColumnCommand(SwerveDriveSubsystem.getSetpoint(Buttonboard.getInstance().getColValue())));
         addCommands(Utils.getAutoScoreCommands(Buttonboard.getInstance().getRowValue()));
     }
