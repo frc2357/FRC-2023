95a1eac87130844d9fc4fa11ac26dac23b101e26
NolanCampbell2357 <nolanc1223@gmail.com>
2023-02-24 15:24:23 -0600

    Add ability to get full trajectory

--- robotbase/src/main/java/com/team2357/frc2023/trajectoryutil/AvailableTeleopTrajectories.java
+++ robotbase/src/main/java/com/team2357/frc2023/trajectoryutil/AvailableTeleopTrajectories.java
@@ -11,14 +11,14 @@ import com.team2357.lib.util.Utility;
 
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.WaitCommand;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 
 public class AvailableTeleopTrajectories {
 
     private enum CONVERGENCE_POINT {
-        OPPOSING_LOADING_STATION(0),
-        CHARGE_STATION(1),
-        CABLE_BUMP(2);
+        NEAR(0), // Point closest to the opposing alliance load station
+        MIDDLE(1), // Point in middle of the field, close to the charge station
+        FAR(2); // Point farthest from the opposing alliance load station
 
         int index;
 
@@ -31,7 +31,7 @@ public class AvailableTeleopTrajectories {
      * key: y-value of start point
      * value: command from start point to convergence point
      */
-    private static HashMap<Double, Command> startToConvergenceTrajMao = new HashMap<Double, Command>();
+    private static HashMap<Double, Command> startToConvergenceTrajMap = new HashMap<Double, Command>();
 
     /*
      * key: y-value of start point
@@ -45,7 +45,7 @@ public class AvailableTeleopTrajectories {
      */
     private static HashMap<Double, CONVERGENCE_POINT> yToConvergenceMap = new HashMap<Double, CONVERGENCE_POINT>();
 
-    private static int numberColumns = 3;
+    private static int numberColumns = 9;
     /*
      * row: convergence point index
      * col: corresponding auto to get to grid column
@@ -61,16 +61,26 @@ public class AvailableTeleopTrajectories {
          * To add another start to convergence trajectory simply do
          * addStartTrajectory(fileName, CONVERGENCE_POINT)
         */ 
-        addStartTrajectory("enterCommunity 4.5", CONVERGENCE_POINT.OPPOSING_LOADING_STATION);
-        addStartTrajectory("enterCommunity 4.65", CONVERGENCE_POINT.OPPOSING_LOADING_STATION);
-        addStartTrajectory("enterCommunity 4.85", CONVERGENCE_POINT.OPPOSING_LOADING_STATION);
+        addStartTrajectory("enterCommunity 4.5", CONVERGENCE_POINT.NEAR);
+        addStartTrajectory("enterCommunity 4.65", CONVERGENCE_POINT.NEAR);
+        addStartTrajectory("enterCommunity 4.85", CONVERGENCE_POINT.NEAR);
 
         // Add all convergence to column trajectories
+        convergenceToColumnTraj = new Command[CONVERGENCE_POINT.values().length][numberColumns];
 
+        addConvergenceToColumnTrajectory("from NEAR to pos0", CONVERGENCE_POINT.NEAR, 0);
+        addConvergenceToColumnTrajectory("from NEAR to pos1", CONVERGENCE_POINT.NEAR, 1);
+        addConvergenceToColumnTrajectory("from NEAR to pos2", CONVERGENCE_POINT.NEAR, 2);
+        addConvergenceToColumnTrajectory("from NEAR to pos3", CONVERGENCE_POINT.NEAR, 3);
+        addConvergenceToColumnTrajectory("from NEAR to pos4", CONVERGENCE_POINT.NEAR, 4);
+        addConvergenceToColumnTrajectory("from NEAR to pos5", CONVERGENCE_POINT.NEAR, 5);
+        addConvergenceToColumnTrajectory("from NEAR to pos6", CONVERGENCE_POINT.NEAR, 6);
+        addConvergenceToColumnTrajectory("from NEAR to pos7", CONVERGENCE_POINT.NEAR, 7);
+        addConvergenceToColumnTrajectory("from NEAR to pos8", CONVERGENCE_POINT.NEAR, 8);
 
         // Load hashmap keys into sorted array for efficient searching
-        yVals = new Double[startToConvergenceTrajMao.keySet().size()];
-        startToConvergenceTrajMao.keySet().toArray(yVals);
+        yVals = new Double[startToConvergenceTrajMap.keySet().size()];
+        startToConvergenceTrajMap.keySet().toArray(yVals);
         Arrays.sort(yVals);
     }
 
@@ -81,19 +91,15 @@ public class AvailableTeleopTrajectories {
                 DriverStationAllianceGetter.getAlliance());
         double yVal = initialState.poseMeters.getY();
 
-        startToConvergenceTrajMao.put(yVal, TrajCmd);
+        startToConvergenceTrajMap.put(yVal, TrajCmd);
         xMap.put(yVal, initialState.poseMeters.getX());
         yToConvergenceMap.put(yVal, toConvergencePoint);
     }
 
-    private static void addConvergenceToColumnTrajectory(String fileName, CONVERGENCE_POINT fromConvergencePoint) {
-        PathPlannerTrajectory trajectory = TrajectoryUtil.createPathPlannerTrajectory(fileName);
-        Command TrajCmd = TrajectoryUtil.createDrivePathCommand(trajectory, false);
-        PathPlannerState initialState = PathPlannerTrajectory.transformStateForAlliance(trajectory.getInitialState(),
-                DriverStationAllianceGetter.getAlliance());
-        double yVal = initialState.poseMeters.getY();
-
-        
+    private static void addConvergenceToColumnTrajectory(String fileName, CONVERGENCE_POINT fromConvergencePoint, int col) {
+        Command trajectoryCmd = TrajectoryUtil.createTrajectoryPathCommand(fileName, false);
+       
+        convergenceToColumnTraj[fromConvergencePoint.index][col] = trajectoryCmd;
     }
 
     /**
@@ -104,8 +110,12 @@ public class AvailableTeleopTrajectories {
      */
     public static Command BuildTrajectory(int col, Pose2d pose) {
         double startKey = getTrajectoryKey(pose);
-        Command startCommand = startToConvergenceTrajMao.get(startKey);
-        return new WaitCommand(0);
+        Command startCommand = startToConvergenceTrajMap.get(startKey);
+
+        CONVERGENCE_POINT convergencePoint = yToConvergenceMap.get(startKey);
+        Command toColCommand = convergenceToColumnTraj[convergencePoint.index][col];
+
+        return new SequentialCommandGroup(startCommand, toColCommand);
     }
 
     /**
