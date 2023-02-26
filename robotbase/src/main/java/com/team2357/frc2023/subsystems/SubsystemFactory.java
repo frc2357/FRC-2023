8efcae2a00f21dc8089069fe352c2150d0d8c1df
Tyson453 <95186148+Tyson453@users.noreply.github.com>
2023-02-25 20:58:01 -0800

    Cleanup SubsystemFactory

--- robotbase/src/main/java/com/team2357/frc2023/subsystems/SubsystemFactory.java
+++ robotbase/src/main/java/com/team2357/frc2023/subsystems/SubsystemFactory.java
@@ -1,72 +1,9 @@
 package com.team2357.frc2023.subsystems;
 
-import com.ctre.phoenix.motorcontrol.can.TalonSRX;
-import com.ctre.phoenix.sensors.WPI_Pigeon2;
-import com.revrobotics.CANSparkMax;
-import com.revrobotics.CANSparkMaxLowLevel.MotorType;
-import com.swervedrivespecialties.swervelib.Mk4iSwerveModuleHelper;
-import com.swervedrivespecialties.swervelib.SwerveModule;
 import com.team2357.frc2023.Constants;
-
-import edu.wpi.first.wpilibj.DoubleSolenoid;
-import edu.wpi.first.wpilibj.PneumaticsModuleType;
-import edu.wpi.first.wpilibj.shuffleboard.BuiltInLayouts;
-import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
-import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 
 public class SubsystemFactory {
         public SwerveDriveSubsystem CreateSwerveDriveSubsystem() {
-                // ShuffleboardTab tab = Shuffleboard.getTab(Constants.DRIVE.SWERVE_MODULE_SHUFFLEBOARD_TAB_NAME);
-
-                // WPI_Pigeon2 pigeon = new WPI_Pigeon2(Constants.CAN_ID.PIGEON_ID, Constants.CAN_ID.DRIVE_CANBUS);
-
-                // SwerveModule frontLeft = Mk4iSwerveModuleHelper.createFalcon500(
-                //                 tab.getLayout("Front Left Module", BuiltInLayouts.kList)
-                //                                 .withSize(2, 4)
-                //                                 .withPosition(0, 0),
-                //                 Mk4iSwerveModuleHelper.GearRatio.L2,
-                //                 Constants.CAN_ID.FRONT_LEFT_MODULE_DRIVE_MOTOR_ID,
-                //                 Constants.CAN_ID.FRONT_LEFT_MODULE_STEER_MOTOR_ID,
-                //                 Constants.CAN_ID.FRONT_LEFT_MODULE_STEER_ENCODER_ID,
-                //                 // the canbus the module is on
-                //                 Constants.CAN_ID.DRIVE_CANBUS,
-                //                 Constants.DRIVE.FRONT_LEFT_MODULE_STEER_OFFSET);
-
-                // SwerveModule frontRight = Mk4iSwerveModuleHelper.createFalcon500(
-                //                 tab.getLayout("Front Right Module", BuiltInLayouts.kList)
-                //                                 .withSize(2, 4)
-                //                                 .withPosition(2, 0),
-                //                 Mk4iSwerveModuleHelper.GearRatio.L2,
-                //                 Constants.CAN_ID.FRONT_RIGHT_MODULE_DRIVE_MOTOR_ID,
-                //                 Constants.CAN_ID.FRONT_RIGHT_MODULE_STEER_MOTOR_ID,
-                //                 Constants.CAN_ID.FRONT_RIGHT_MODULE_STEER_ENCODER_ID,
-                //                 // the canbus the module is on
-                //                 Constants.CAN_ID.DRIVE_CANBUS,
-                //                 Constants.DRIVE.FRONT_RIGHT_MODULE_STEER_OFFSET);
-
-                // SwerveModule backLeft = Mk4iSwerveModuleHelper.createFalcon500(
-                //                 tab.getLayout("Back Left Module", BuiltInLayouts.kList)
-                //                                 .withSize(2, 4)
-                //                                 .withPosition(4, 0),
-                //                 Mk4iSwerveModuleHelper.GearRatio.L2,
-                //                 Constants.CAN_ID.BACK_LEFT_MODULE_DRIVE_MOTOR_ID,
-                //                 Constants.CAN_ID.BACK_LEFT_MODULE_STEER_MOTOR_ID,
-                //                 Constants.CAN_ID.BACK_LEFT_MODULE_STEER_ENCODER_ID,
-                //                 // the canbus the module is on
-                //                 Constants.CAN_ID.DRIVE_CANBUS,
-                //                 Constants.DRIVE.BACK_LEFT_MODULE_STEER_OFFSET);
-
-                // SwerveModule backRight = Mk4iSwerveModuleHelper.createFalcon500(
-                //                 tab.getLayout("Back Right Module", BuiltInLayouts.kList)
-                //                                 .withSize(2, 4)
-                //                                 .withPosition(6, 0),
-                //                 Mk4iSwerveModuleHelper.GearRatio.L2,
-                //                 Constants.CAN_ID.BACK_RIGHT_MODULE_DRIVE_MOTOR_ID,
-                //                 Constants.CAN_ID.BACK_RIGHT_MODULE_STEER_MOTOR_ID,
-                //                 Constants.CAN_ID.BACK_RIGHT_MODULE_STEER_ENCODER_ID,
-                //                 // the canbus the module is on
-                //                 Constants.CAN_ID.DRIVE_CANBUS,
-                //                 Constants.DRIVE.BACK_RIGHT_MODULE_STEER_OFFSET);
 
                 SwerveDriveSubsystem subsystem = new SwerveDriveSubsystem(
                         Constants.CAN_ID.PIGEON_ID,
