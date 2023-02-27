948280f0429c2ae15529ce2ae60d7800328ef753
coderkevin <kevin@redfenix.com>
2023-02-27 12:46:09 -0600

    Add network tables test client and server
    This adds a test mode for both client and server that can be
    used with each other to verify operation of network tables libraries

--- buttonboard/app/src/main/java/buttonboard/App.java
+++ buttonboard/app/src/main/java/buttonboard/App.java
@@ -3,12 +3,76 @@
  */
 package buttonboard;
 
+import java.lang.Math;
+import buttonboard.ArduinoUSB;
+import buttonboard.NetworkTablesClient;
+import buttonboard.NetworkTablesServer;
+
 public class App {
-    public String getGreeting() {
-        return "Hello World!";
-    }
+    private static final String ARG_TEST_ARDUINO = "test:arduino";
+    private static final String ARG_TEST_NTCLIENT = "test:ntclient";
+    private static final String ARG_TEST_NTSERVER = "test:ntserver";
 
     public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+        try {
+            if (args.length > 0 && args[0].length() > 0) {
+                switch(args[0]) {
+                    case ARG_TEST_ARDUINO:
+                        testArduino();
+                        System.exit(0);
+                    case ARG_TEST_NTCLIENT:
+                        testNetworkTablesClient();
+                        System.exit(0);
+                    case ARG_TEST_NTSERVER:
+                        testNetworkTablesServer();
+                        System.exit(0);
+                    default:
+                        System.err.println("Unrecognized command: '" + args[0] + "'");
+                }
+            }
+        } catch (Exception e) {
+            System.err.println("***");
+            System.err.println("Unhandled exception: " + e.getMessage());
+            e.printStackTrace();
+            System.err.println("***");
+        }
+    }
+
+    private static void testArduino() {
+        ArduinoUSB.scan();
+    }
+
+    private static void testNetworkTablesClient() {
+        NetworkTablesClient nt = new NetworkTablesClient();
+        nt.open();
+        try {
+            System.out.println("Client ready");
+            while (true) {
+                long x = (long)(Math.random() * 9);
+                long y = (long)(Math.random() * 3);
+                System.out.println("Grid target x=" + x + ", y=" + y);
+                nt.setGridTarget(x, y);
+                Thread.sleep(1000);
+            }
+        } catch (InterruptedException ie) {
+            System.out.println("Client exiting");
+            nt.close();
+            return;
+        }
+    }
+
+    private static void testNetworkTablesServer() {
+        NetworkTablesServer nt = new NetworkTablesServer();
+        nt.open();
+        try {
+            while (true) {
+                System.out.println("Grid target x=" + nt.getGridX() + ", y=" + nt.getGridY());
+                Thread.sleep(1000);
+            }
+        } catch (InterruptedException ie) {
+            System.out.println("Server exiting");
+            nt.close();
+            return;
+        }
     }
 }
