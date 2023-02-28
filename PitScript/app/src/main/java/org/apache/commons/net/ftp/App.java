8111ecd4d51001e3f5fc0127c91eece8ab9d8a61
MidgleyM <115593302+MidgleyM@users.noreply.github.com>
2023-02-28 15:42:08 -0600

    started to get things done a bit

--- PitScript/app/src/main/java/org/apache/commons/net/ftp/App.java
+++
@@ -1,8 +0,0 @@
-package org.apache.commons.net.ftp;
-import org.apache.commons.net.ftp.FTPClient;
-public class App {
-    public  FTPClient ftp = new FTPClient();
-    public  void main() {
-        ftp.connect();
-    }
-}
