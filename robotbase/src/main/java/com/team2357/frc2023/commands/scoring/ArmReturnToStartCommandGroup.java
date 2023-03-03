d6a133ea93ac68b3ec534eaec4efc223c8b34df4
NolanCampbell2357 <nolanc1223@gmail.com>
2023-03-02 20:12:55 -0600

    Fix return command

--- robotbase/src/main/java/com/team2357/frc2023/commands/scoring/ArmReturnToStartCommandGroup.java
+++ robotbase/src/main/java/com/team2357/frc2023/commands/scoring/ArmReturnToStartCommandGroup.java
@@ -10,7 +10,7 @@ import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 public class ArmReturnToStartCommandGroup extends SequentialCommandGroup {
     public ArmReturnToStartCommandGroup() {
         addCommands(new CloseClawCommand());
-        addCommands(new ArmRotateToPositionCommand(Constants.ARM_ROTATION.RETRACTED_ROTATIONS));
         addCommands(new ArmRetractToPositionCommand(Constants.ARM_EXTENSION.RETRACTED_ROTATIONS));
+        addCommands(new ArmRotateToPositionCommand(Constants.ARM_ROTATION.RETRACTED_ROTATIONS));
     }
 }
