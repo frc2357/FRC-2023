960a0ecc5e5aaf640fe7eef201d6a4637b7a8128
Charlie Haning <legokid1980@outlook.com>
2023-03-05 10:25:27 -0600

    various cleanups

--- wpilibpi/calibration.py
+++ wpilibpi/calibration.py
@@ -7,7 +7,7 @@ import json
 import numpy as np
 from functools import partial
 
-cam0_json="""{
+cam0_json = """{
     "camera": "SPCA2688 AV Camera (1bcf:0b15)",
     "platform": "Windows NT 10.0; Win64; x64",
     "camera_matrix": {
@@ -55,76 +55,83 @@ cam0_json="""{
     "keyframes": 29,
     "calibration_time": "Tue, 14 Feb 2023 02:27:37 GMT"
 }"""
-#cal_cam0={"mtx":[1028.90904278,0.,638.57001085,0., 1028.49276415,337.36382032,0., 0., 1.],
+# cal_cam0={"mtx":[1028.90904278,0.,638.57001085,0., 1028.49276415,337.36382032,0., 0., 1.],
 #    "dst":[-0.028306160076120947, 0.10007185327022922, -0.0008945602599457421, -0.003429637337026977, -0.37368732957571205],
 #    "img_size":[1280,720],
 #    "newmtx":[964.6962890625, 0.0, 629.3880578097305,0.0, 962.7504272460938, 332.8399478557767,0.0, 0.0, 1.0],
 #    "roi":[23, 17, 1224, 676]
 #    }
 
+
 class CameraCalibration:
-    """ 
+    """
     Class to hold Calibration Coefficients and mapped undistort function
     Can load calibration info from json file
     """
-    def __init__(self,mtx,dst,img_size=(1280,720),newmtx=None,roi=None):
+
+    def __init__(self, mtx, dst, img_size=(1280, 720), newmtx=None, roi=None):
         self.mtx = mtx
         self.dst = dst
         self.img_size = img_size
         self.newmtx = newmtx
         if roi is None:
-            self.roi = [0,0,img_size[0],img_size[1]]
+            self.roi = [0, 0, img_size[0], img_size[1]]
 
-    def undistort(self,img):
-        #cv.undistort(	src, cameraMatrix, distCoeffs[, dst[, newCameraMatrix]]	) ->	dst
+    def undistort(self, img):
+        # cv.undistort(	src, cameraMatrix, distCoeffs[, dst[, newCameraMatrix]]	) ->	dst
         dest = None
-        ret = cv2.undistort(img,self.mtx,self.dst,dest,self.newmtx)
-        x, y, w, h = self.roi 
-        return ret[y:y+h, x:x+w]
+        ret = cv2.undistort(img, self.mtx, self.dst, dest, self.newmtx)
+        x, y, w, h = self.roi
+        return ret[y : y + h, x : x + w]
 
-    def projectPoints(self, pts, rvec=[0, 0, 0], tvec=[0, 0, 0] ):
-        #cv.projectPoints(	objectPoints, rvec, tvec, cameraMatrix, distCoeffs[, imagePoints[, jacobian[, aspectRatio]]]	) ->	imagePoints, jacobian
+    def projectPoints(self, pts, rvec=[0, 0, 0], tvec=[0, 0, 0]):
+        # cv.projectPoints(	objectPoints, rvec, tvec, cameraMatrix, distCoeffs[, imagePoints[, jacobian[, aspectRatio]]]	) ->	imagePoints, jacobian
         return cv2.projectPoints(pts, rvec, tvec, self.mtx, self.dst)
-    
+
     def to_json(self):
-        return json.dumps({"mtx":self.mtx.ravel(),
-                "dst":self.dst.ravel(),
-                "img_size":[1280,720],
-                "newmtx":self.newmtx.ravel(),
-                "roi": self.roi})
+        return json.dumps(
+            {
+                "mtx": self.mtx.ravel(),
+                "dst": self.dst.ravel(),
+                "img_size": [1280, 720],
+                "newmtx": self.newmtx.ravel(),
+                "roi": self.roi,
+            }
+        )
 
     @staticmethod
     def load_cal_json(jfile):
-        """ json calibration file parser
-            this is a direct json import based on to_json format
+        """json calibration file parser
+        this is a direct json import based on to_json format
         """
-        j = json.loads(open(jfile,'r').read())
-        mtx = np.array(j['mtx']).reshape(3,3)
-        dst = np.array(j['dst'])
-        img_size = j['img_size']
-        newmtx = np.array(j['newmtx']).reshape(3,3)
-        roi = j['roi']
-        return CameraCalibration(mtx,dst,img_size,newmtx,roi)
+        j = json.loads(open(jfile, "r").read())
+        mtx = np.array(j["mtx"]).reshape(3, 3)
+        dst = np.array(j["dst"])
+        img_size = j["img_size"]
+        newmtx = np.array(j["newmtx"]).reshape(3, 3)
+        roi = j["roi"]
+        return CameraCalibration(mtx, dst, img_size, newmtx, roi)
 
     @staticmethod
     def load_cal_json_calibdb(jstr):
-        """  calibdb.net json output parser
-            
-            Args:
-                jstr: json calibration string from calibdb.net
+        """calibdb.net json output parser
 
-            Returns:
-                CameraCalibration object
+        Args:
+            jstr: json calibration string from calibdb.net
+
+        Returns:
+            CameraCalibration object
         """
         j = json.loads(jstr)
-        mtx = np.array(j['camera_matrix']['data']).reshape(3,3)
-        dst = np.array(j['distortion_coefficients']['data'])
-        img_size = j['img_size']['data']
-        newmtx,roi = cv2.getOptimalNewCameraMatrix(mtx,dst,img_size,1,img_size)
+        mtx = np.array(j["camera_matrix"]["data"]).reshape(3, 3)
+        dst = np.array(j["distortion_coefficients"]["data"])
+        img_size = j["img_size"]["data"]
+        newmtx, roi = cv2.getOptimalNewCameraMatrix(mtx, dst, img_size, 1, img_size)
         return CameraCalibration(mtx, dst, img_size, newmtx, roi)
 
-# create image_cal and cam0 CameraCalibration instances on import
-image_cal = CameraCalibration(mtx = np.array([1000, 0, 1280/2,0,1000,720/2,0,0,1]).reshape(3,3),dst=np.float32([0,0,0,0]))
-cam0 = CameraCalibration.load_cal_json("left_camera_cal.json") # load calibration data in CameraCalibration class
 
-   
+# create image_cal and cam0 CameraCalibration instances on import
+image_cal = CameraCalibration(
+    mtx=np.array([1000, 0, 1280 / 2, 0, 1000, 720 / 2, 0, 0, 1]).reshape(3, 3), dst=np.float32([0, 0, 0, 0])
+)
+cam0 = CameraCalibration.load_cal_json("left_camera_cal.json")  # load calibration data in CameraCalibration class
--- wpilibpi/run_vision.py
+++ wpilibpi/run_vision.py
@@ -1,8 +1,20 @@
 #!/usr/bin/env python3
 
+# originally from:
 # Copyright (c) FIRST and other WPILib contributors.
 # Open Source Software; you can modify and/or share it under the terms of
 # the WPILib BSD license file in the root directory of this project.
+#
+# modified and extended by a mentor and other team members of
+# Team 2357  Raymore-Peculiar High School
+#
+# ██████  ██████  ███████ ███████
+#     ██      ██ ██           ██
+# █████   █████  ███████     ██
+# ██           ██      ██    ██
+# ███████ ██████  ███████    ██
+# text art (just because it's cool) check out: https://patorjk.com/software/taag
+
 import copy
 import json
 import logging
@@ -11,16 +23,13 @@ import time
 
 import cv2
 import numpy as np
-from wpimath.geometry import Transform3d
-
 import apriltag_funcs
+import detect_colors
+from calibration import cam0, image_cal  # bandaid for
 
 # class used to encapsulate Camera + Calibration information
 from cameravision import CameraVision
-
-import detect_colors
-from calibration import image_cal, cam0 #bandaid for 
-from pprint import pprint
+from gamepiece import GamePieceTracker
 
 log = logging.getLogger(__name__)
 log.addHandler(logging.NullHandler())
@@ -34,77 +43,81 @@ if __name__ == "__main__":
         cfgfile = str(sys.argv[1])
     if len(sys.argv) > 2:
         simulate = True
-    camvis = CameraVision(cfgfile, simulate=simulate) # this class automatically creates all camera + calibration objects
+    # this class automatically creates all camera + calibration objects
+    camvis = CameraVision(cfgfile, simulate=simulate)
     cam0 = camvis.cameras[0]
-    #configure networks tables
-    nt_table = camvis.ntinst.getTable("wpiblipi")
-    apriltag_json_NT = nt_table.getStringTopic("apriltag_js").publish()
-    gamepiece_json_NT = nt_table.getStringTopic("gamepiece_js").publish()
+    # cam1 = camvis.cameras[1]
+    # configure networks tables
+    nt_table = camvis.ntinst.getTable("gridcam")
+    apriltag_NT = nt_table.getStringTopic("tags").publish()
+    # gamepiece_NT = nt_table.getStringArrayTopic("grid").getEntry(["*", "*", "*"])
     log.info(f'Camera width = {cam0.config["width"]}, {cam0.config["height"]}')
     frame = np.zeros(shape=(cam0.config["height"], cam0.config["width"], 3), dtype=np.uint8)
+
     # retrieve the camera calibration information needed by apriltags
-    Fx = cam0.cal.mtx[0,0]
-    Fy = cam0.cal.mtx[1,1]
-    Cx = cam0.cal.mtx[0,2]
-    Cy = cam0.cal.mtx[1,2]
-    apriltag = apriltag_funcs.AprilTagPipeline((1280,720),Fx,Fy,Cx,Cy)
-    gamepiece = detect_colors.GamePieceDetector()
-    
+    Fx = cam0.cal.mtx[0, 0]
+    Fy = cam0.cal.mtx[1, 1]
+    Cx = cam0.cal.mtx[0, 2]
+    Cy = cam0.cal.mtx[1, 2]
+
+    apriltag = apriltag_funcs.AprilTagPipeline((1280, 720), Fx, Fy, Cx, Cy)
+    # apriltag.register_NT_vars(nt_table)
+    gpdetector = detect_colors.GamePieceDetector()
+    gamepieces = GamePieceTracker  # this is a singleton
+    # gamepieces.register_NT_vars(nt_table)
+
     count = 0
     avg = 0
     while True:
 
         start = time.perf_counter()
-        
+
         # grab a new frame or disk image
         if simulate:
             # make sure to make a copy of the original image
-            frame = copy.copy(cam0.images[count%(len(cam0.images))])
+            frame = copy.copy(cam0.images[count % (len(cam0.images))])
         else:
-            #TODO: do I grab multiple cameras manually here, or iterate through cameras in camvis.cameras?
+            # TODO: do I grab multiple cameras manually here, or iterate through cameras in camvis.cameras?
             #      perhaps even storing the frame in the CameraObject?
             cam0.sink.grabFrame(frame)
-        
-        #get server time for which last frame was captured
+
+        # get server time for which last frame was captured
         timeoffset = camvis.ntinst.getServerTimeOffset()
-        orig = copy.copy(frame) # create a copy for color detection
+        orig = copy.copy(frame)  # create a copy for color detection
         try:
             # trying undistort seemed to cause lockup issues???
-            #frame = cam0.cal.undistort(frame)
-            #TODO: do we care if image is undistorted -- asked another way --> is AprilTags using the camera matrix values to 
+            # frame = cam0.cal.undistort(frame)
+            # TODO: do we care if image is undistorted -- asked another way --> is AprilTags using the camera matrix values to
             #      undistort?  Answer is assumed to be YES, which means doing the undistort() call above is a bad idea
-            #if simulate:
+            # if simulate:
             #    frame = cv2.putText(frame,count%(len(cam0.images)),cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,0), 3)
 
-            try:
-                i = 0
-                frame,roi_rects = apriltag.runPipeline(frame,image_cal)
-                orig = gamepiece.runPipeline(orig, roi_rects, colorize=0.5)
-                j = apriltag.to_json()
-                j['timestamp'] = timeoffset 
-                log.debug(f"Timestamp: {j['timestamp']}")
-                apriltag_json_NT.set(json.dumps(j,separators=(',',':')))                # send data to networks table
-                gamepiece_json_NT.set(gamepiece.to_json())  # send data to networks table
-            except Exception as e:
-                log.exception(e)
-            ###
-            #trying to overlay detected colors
-            frame = cv2.addWeighted(frame,0.5,orig,0.5,0)
+            # try:
+            i = 0
+            frame, roi_rects = apriltag.runPipeline(frame, image_cal)
+            orig = gpdetector.runPipeline(orig, roi_rects, colorize=0.5)
+            j = dict()
+            j["timestamp"] = timeoffset
+            j["tags"] = apriltag.to_dict()
+            log.debug(f"Timestamp: {j['timestamp']}")
+            apriltag_NT.set(json.dumps(j, separators=(",", ":")))  # send data to networks table
+
+            # trying to overlay detected colors
+            frame = cv2.addWeighted(frame, 0.5, orig, 0.5, 0)
             cam0.outstream.putFrame(frame)
 
-            # track processing time.  
+            # track processing time, average ten updates.
             end = time.perf_counter()
-            avg += end-start
+            avg += end - start
             count += 1
-            if(count%10 == 0):
+            if count % 10 == 0:
                 logging.info(f"Processing took Avg: {avg/10.0:.4f} sec")
                 avg = 0
-            if simulate: # the following slows down image processing w/out messing up web server function
-                for i in range(0,30):
+            if simulate:  # the following slows down image processing w/out messing up web server function
+                for i in range(0, 30):
                     cam0.outstream.putFrame(frame)
-                    time.sleep(0.03)
+                    time.sleep(0.05)
         except Exception as e:
-            #raise(e)
+            # raise(e)
             log.exception(f"ERROR: {e}")
             pass
-            
