5fd561b6d612aeb537eac64fbe906d9c2c9f4be1
NolanCampbell2357 <nolanc1223@gmail.com>
2023-03-10 17:02:09 -0600

    Update to use top left as 0,0

--- robotbase/src/main/java/com/team2357/frc2023/util/Utility.java
+++ robotbase/src/main/java/com/team2357/frc2023/util/Utility.java
@@ -8,17 +8,16 @@ public class Utility {
     /**
      * 
      * @param column The column from 0-8 with 0 being closest to opposing alliance
-     *               loading station
      * @return The id of the april tag on the grid
      */
     public static int gridColumnToAprilTagID(int column) {
-        switch (column % 3) {
+        switch (column / 3) {
             case 0:
-                return Buttonboard.getInstance().getAlliance() == Alliance.Blue ? 6 : 3;
+                return Buttonboard.getInstance().getAlliance() == Alliance.Blue ? 8 : 1;
             case 1:
                 return Buttonboard.getInstance().getAlliance() == Alliance.Blue ? 7 : 2;
             case 2:
-                return Buttonboard.getInstance().getAlliance() == Alliance.Blue ? 8 : 1;
+                return Buttonboard.getInstance().getAlliance() == Alliance.Blue ? 6 : 3;
             default:
                 return -1;
         }
