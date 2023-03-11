9bd3996ca7bbac7516706bf4172c40c0093fce12
MidgleyM <115593302+MidgleyM@users.noreply.github.com>
2023-03-11 15:12:46 -0600

    fixed mispelling

--- robotbase/src/main/java/com/team2357/frc2023/commands/util/AmpZeroCommandGroup.java
+++ robotbase/src/main/java/com/team2357/frc2023/commands/util/AmpZeroCommandGroup.java
@@ -5,13 +5,10 @@ import com.team2357.frc2023.commands.armrotation.ArmRotationAmpZeroCommand;
 import com.team2357.frc2023.commands.intake.WinchAmpZeroCommand;
 import com.team2357.frc2023.commands.armextension.ArmExtendAmpZeroCommand;
 
-import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.ParallelDeadlineGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
 
 public class AmpZeroCommandGroup extends ParallelDeadlineGroup{
-    static Command whinchZero = new WinchAmpZeroCommand();
-    static Command armRotationZero = new ArmRotationAmpZeroCommand();
 
     public AmpZeroCommandGroup() {
         super(new WaitCommand(Constants.AMP_ZERO.AMP_ZERO_DEADLINE_SECONDS), new WinchAmpZeroCommand(), new ArmRotationAmpZeroCommand(), new ArmExtendAmpZeroCommand());
