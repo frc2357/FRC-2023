bebcfd627a62d32d7a13e8c56801baafd0075a87
MidgleyM <115593302+MidgleyM@users.noreply.github.com>
2023-03-11 15:35:30 -0600

    made it wait before it stows

--- robotbase/src/main/java/com/team2357/frc2023/commands/intake/IntakeStowCommandGroup.java
+++ robotbase/src/main/java/com/team2357/frc2023/commands/intake/IntakeStowCommandGroup.java
@@ -6,13 +6,15 @@ import com.team2357.frc2023.subsystems.DualLimelightManagerSubsystem;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
 
-public class IntakeStowCommandGroup extends ParallelCommandGroup {
+public class IntakeStowCommandGroup extends SequentialCommandGroup {
     public IntakeStowCommandGroup() {
         addCommands(new InstantCommand(() -> DualLimelightManagerSubsystem.getInstance().setAprilTagPipelineActive()));
 
         addCommands(new SequentialCommandGroup(
                 new ParallelCommandGroup(
+                        new WaitCommand(0.25),
                         new IntakeRollerStopCommand(),
                         new IntakeArmStowCommand()),
 
