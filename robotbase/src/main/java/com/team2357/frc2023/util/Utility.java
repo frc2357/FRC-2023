c62d2cdaecf01928e35603e6657e3e7223c4ad7a
NolanCampbell2357 <nolanc1223@gmail.com>
2023-03-21 20:14:41 -0500

    Copy in auto-targeting

--- robotbase/src/main/java/com/team2357/frc2023/util/Utility.java
+++ robotbase/src/main/java/com/team2357/frc2023/util/Utility.java
@@ -2,6 +2,9 @@ package com.team2357.frc2023.util;
 
 import com.team2357.frc2023.networktables.Buttonboard;
 
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Transform2d;
+import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
 
 public class Utility {
@@ -22,4 +25,17 @@ public class Utility {
                 return -1;
         }
     }
+
+    
+  /**
+   * From: https://github.com/Mechanical-Advantage/RobotCode2023/blob/main/src/main/java/org/littletonrobotics/frc2023/util/GeomUtil.java
+   * Creates a pure translating transform
+   *
+   * @param x The x componenet of the translation
+   * @param y The y componenet of the translation
+   * @return The resulting transform
+   */
+  public static Transform2d translationToTransform(double x, double y) {
+    return new Transform2d(new Translation2d(x, y), new Rotation2d());
+  }
 }
