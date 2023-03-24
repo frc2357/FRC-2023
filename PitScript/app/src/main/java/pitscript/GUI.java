590f4e09b5812bab2c7e49a9d61ffa22c6654deb
MidgleyM <115593302+MidgleyM@users.noreply.github.com>
2023-03-23 19:19:59 -0500

    Better pit script (#268)
    * made the pitScript better with more control, needs to be tested though
    
    * made the text box actually update now! and the thing turns yellow so you know that it was pressed

--- PitScript/app/src/main/java/pitscript/GUI.java
+++ PitScript/app/src/main/java/pitscript/GUI.java
@@ -6,11 +6,12 @@ import javax.swing.JFrame;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.ScrollPaneConstants;
+import javax.swing.SwingWorker;
 
 public class GUI extends JButton {
     public static JTextArea text = new JTextArea("No operation performed",1000,1000);
     public static JFrame frame;
-    public static JButton button = new GUI("Transfer Files");
+    public static JButton transferButton = new GUI("Transfer Files");
     public static JScrollPane scroll = new JScrollPane(text);
     public static JButton endButton = new JButton("End program");
 
@@ -19,21 +20,36 @@ public class GUI extends JButton {
     }
 
     public static void writeToText(String message) {
-        text.setEditable(true);
         text.setText(message);
-        text.setEditable(false);
+        text.updateUI();
     }
 
-    public static void buttonPressed() {
-        if (App.TransferFiles()) {
-            button.setBackground(Color.RED);
+    public static void transferButtonPressed() {
+        new SwingWorker<Boolean , Object>(){
+            public Boolean doInBackground(){
+            transferButton.setBackground(Color.YELLOW);
+        if (!App.TransferFiles()) {
+            transferButton.setBackground(Color.RED);
+            return false;
         }
         else{
-            button.setBackground(Color.GREEN);
+            transferButton.setBackground(Color.GREEN);
+            return true;
         }
+            }
+   
+            public void done(){
+
+            }
+       }.execute();
+        
     }
-    public static void killProgram(){
-        System.exit(0);
+
+
+    public static void killSwitch(){
+        System.out.println("program go bye bye because kill switch was used.");
+        GUI.writeToText("kill switch was desired, so program has been poofed.");
+        System.exit(ABORT);
     }
 
     public static void MakeGUI() {
@@ -41,15 +57,15 @@ public class GUI extends JButton {
         frame.setLayout(null);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(400, 400);
-        frame.add(button);
+        frame.add(transferButton);
         frame.add(endButton);
         frame.add(scroll);
         scroll.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
-        button.setBounds(0, 0, 145, 145);
-        endButton.setBounds(241, 0, 145, 145);
-        button.addActionListener(e -> GUI.buttonPressed());
-        endButton.addActionListener(e -> GUI.killProgram());
-        text.setEditable(false);
+        transferButton.setBounds(0, 0, 145, 145);
+        endButton.setBounds(241,0,145,145);
+        transferButton.addActionListener(e -> GUI.transferButtonPressed());
+        endButton.addActionListener(e -> GUI.killSwitch());
+        text.setEditable(true);
         text.setLineWrap(true);
         scroll.setBounds(0, 145, 750, 500);
         frame.setVisible(true);
