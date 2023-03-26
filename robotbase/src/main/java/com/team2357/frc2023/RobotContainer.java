668bcd0456c00eb455dbb1190df69496b24fbacf
NolanCampbell2357 <nolanc1223@gmail.com>
2023-03-26 14:14:32 -0500

    Basic led control (#274)
    * Basic leds
    
    * Basic leds
    
    * Enable compressor

--- robotbase/src/main/java/com/team2357/frc2023/RobotContainer.java
+++ robotbase/src/main/java/com/team2357/frc2023/RobotContainer.java
@@ -8,6 +8,7 @@ import com.pathplanner.lib.server.PathPlannerServer;
 import com.team2357.frc2023.commands.drive.DefaultDriveCommand;
 import com.team2357.frc2023.controls.GunnerControls;
 import com.team2357.frc2023.controls.SwerveDriveControls;
+import com.team2357.frc2023.led.GamepieceLED;
 import com.team2357.frc2023.shuffleboard.AutoCommandChooser;
 import com.team2357.frc2023.subsystems.DualLimelightManagerSubsystem;
 import com.team2357.frc2023.subsystems.SubsystemFactory;
@@ -66,13 +67,16 @@ public class RobotContainer {
 
     // Set default pipeline
     DualLimelightManagerSubsystem.getInstance().setAprilTagPipelineActive();
-    
+
     // Setup compressor
     m_compressor = new Compressor(Constants.CAN_ID.PNEUMATICS_HUB_ID, PneumaticsModuleType.REVPH);
     m_compressor.enableAnalog(Constants.COMPRESSOR.MIN_PRESSURE_PSI,
-    Constants.COMPRESSOR.MAX_PRESSURE_PSI);
-    //m_compressor.disable();
-    
+        Constants.COMPRESSOR.MAX_PRESSURE_PSI);
+    // m_compressor.disable();
+
+    // Init LEDs
+    new GamepieceLED(Constants.GAMEPIECE_LED.PWM_PORT);
+
     // Build trajectory paths
     AvailableTrajectories.generateTrajectories();
     AvailableTrajectoryCommands.generateTrajectories();
@@ -81,7 +85,8 @@ public class RobotContainer {
     // Configure Shuffleboard
     configureShuffleboard();
 
-   // PathPlannerServer.startServer(5811); // 5811 = port number. adjust this according to your needs
+    // PathPlannerServer.startServer(5811); // 5811 = port number. adjust this
+    // according to your needs
   }
 
   /**
