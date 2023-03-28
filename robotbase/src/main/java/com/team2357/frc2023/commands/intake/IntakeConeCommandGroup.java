afa958b02e7fe4b6a694c8069195759e423c8cf1
Tyson453 <95186148+Tyson453@users.noreply.github.com>
2023-03-28 16:32:22 -0700

    Tune intake and wrist

--- robotbase/src/main/java/com/team2357/frc2023/commands/intake/IntakeConeCommandGroup.java
+++ robotbase/src/main/java/com/team2357/frc2023/commands/intake/IntakeConeCommandGroup.java
@@ -2,15 +2,23 @@ package com.team2357.frc2023.commands.intake;
 
 import edu.wpi.first.wpilibj2.command.ParallelDeadlineGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
 
 public class IntakeConeCommandGroup extends SequentialCommandGroup {
     public IntakeConeCommandGroup() {
         super(
-            new ParallelDeadlineGroup(
-                new IntakeStallCommand(),
-                new IntakeDeployConeCommandGroup()
-            ),
-            new IntakeStowConeCommandGroup()
+            new SequentialCommandGroup(
+                new ParallelDeadlineGroup(
+                    new IntakeStallCommand(),
+                    new IntakeDeployConeCommandGroup()
+                )
+            ).finallyDo((interrupted) -> {
+                new ParallelDeadlineGroup(
+                    new WaitCommand(2), 
+                    new IntakeStowConeCommandGroup()
+                ).schedule();
+            })
         );
+        
     }
 }
--- robotbase/src/main/java/com/team2357/frc2023/commands/intake/IntakeCubeCommandGroup.java
+++ robotbase/src/main/java/com/team2357/frc2023/commands/intake/IntakeCubeCommandGroup.java
@@ -2,15 +2,22 @@ package com.team2357.frc2023.commands.intake;
 
 import edu.wpi.first.wpilibj2.command.ParallelDeadlineGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
 
 public class IntakeCubeCommandGroup extends SequentialCommandGroup {
     public IntakeCubeCommandGroup() {
         super(
-            new ParallelDeadlineGroup(
-                new IntakeStallCommand(),
-                new IntakeDeployCubeCommandGroup()
-            ),
-            new IntakeStowCubeCommandGroup()
+            new SequentialCommandGroup(
+                new ParallelDeadlineGroup(
+                    new IntakeStallCommand(),
+                    new IntakeDeployCubeCommandGroup()
+                )
+            ).finallyDo((interrupted) -> {
+                new ParallelDeadlineGroup(
+                    new WaitCommand(2), 
+                    new IntakeStowCubeCommandGroup()
+                ).schedule();
+            })
         );
     }
 }
