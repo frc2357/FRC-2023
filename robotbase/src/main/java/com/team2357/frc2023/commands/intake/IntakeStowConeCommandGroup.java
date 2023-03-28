b58a4d1289268f9710f9fb77d979cf77ea6fad96
coderkevin <kevin@redfenix.com>
2023-03-28 16:54:17 -0500

    Remove ParallelInterruptCommandGroup
    The parallel interrupt command group was scheduling commands off the group
    which was causing issues with interrupting commands. This removes it
    in favor of using Command.handleInterrupt instead

--- robotbase/src/main/java/com/team2357/frc2023/commands/intake/IntakeStowConeCommandGroup.java
+++ robotbase/src/main/java/com/team2357/frc2023/commands/intake/IntakeStowConeCommandGroup.java
@@ -2,23 +2,22 @@ package com.team2357.frc2023.commands.intake;
 
 import com.team2357.frc2023.commands.everybot.ClawIntakeConeCommand;
 import com.team2357.frc2023.commands.state.SetRobotStateCommand;
-import com.team2357.frc2023.commands.util.ParallelInterruptCommandGroup;
 import com.team2357.frc2023.state.RobotState;
 import com.team2357.frc2023.subsystems.DualLimelightManagerSubsystem;
 
 import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 
 public class IntakeStowConeCommandGroup extends SequentialCommandGroup {
     public IntakeStowConeCommandGroup() {
         addCommands(
-            new ParallelInterruptCommandGroup(
-                new SetRobotStateCommand(RobotState.State.ROBOT_STOWED_EMPTY),
+            new ParallelCommandGroup(
                 new InstantCommand(() -> DualLimelightManagerSubsystem.getInstance().setAprilTagPipelineActive()),
                 new IntakeRollerStopCommand(),
                 new IntakeArmStowCommand(),
                 new ClawIntakeConeCommand()
-            ),
+            ).handleInterrupt(() -> new SetRobotStateCommand(RobotState.State.ROBOT_STOWED_EMPTY).schedule()),
             new SetRobotStateCommand(RobotState.State.ROBOT_STOWED_CONE)
         );
     }
