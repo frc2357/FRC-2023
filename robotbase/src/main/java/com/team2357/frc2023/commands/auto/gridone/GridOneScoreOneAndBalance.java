8a8a0b1e805a66c8d112e29dd96560376006c9b9
NolanCampbell2357 <nolanc1223@gmail.com>
2023-03-30 20:03:33 -0500

    Tweak one score

--- robotbase/src/main/java/com/team2357/frc2023/commands/auto/gridone/GridOneScoreOneAndBalance.java
+++ robotbase/src/main/java/com/team2357/frc2023/commands/auto/gridone/GridOneScoreOneAndBalance.java
@@ -2,8 +2,10 @@ package com.team2357.frc2023.commands.auto.gridone;
 
 import com.team2357.frc2023.commands.intake.IntakeArmRotateDumbCommand;
 import com.team2357.frc2023.commands.intake.IntakeRollerRunCommand;
-import com.team2357.frc2023.commands.intake.IntakeSolenoidExtendCommand;
 import com.team2357.frc2023.commands.intake.IntakeStowConeCommandGroup;
+import com.team2357.frc2023.commands.scoring.cone.ConeHighPrePoseCommand;
+import com.team2357.frc2023.commands.scoring.cone.ConeHighScoreCommand;
+import com.team2357.frc2023.commands.util.AutonomousZeroCommand;
 import com.team2357.frc2023.trajectoryutil.TrajectoryUtil;
 
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
@@ -13,31 +15,31 @@ import edu.wpi.first.wpilibj2.command.WaitCommand;
 public class GridOneScoreOneAndBalance extends ParallelCommandGroup {
     public GridOneScoreOneAndBalance() {
         addCommands(
-            new SequentialCommandGroup(
-                // Initialize
-                new IntakeSolenoidExtendCommand(),
+            // new SequentialCommandGroup(
+            //     // Initialize
+            //     new AutonomousZeroCommand(),
+            //     // Score cone high
+            //     new ConeHighPrePoseCommand(),
+            //     new ConeHighScoreCommand(),
 
-                // Score cone high
-                //new ConeAutoScoreHighCommandGroup(true).withTimeout(6.5),
+            //     // Wait until we going over charge station
+            //     new WaitCommand(1.5),
 
-                // Wait until we going over charge station
-                new WaitCommand(1.5),
+            //     // Then deploy intake after we're back on the floor
+            //     new ParallelCommandGroup(
+            //       // new ClawInstantOpenCommand(),
+            //       new IntakeArmRotateDumbCommand(0.6).withTimeout(2.0),
+            //       new SequentialCommandGroup(
+            //         new IntakeRollerRunCommand().withTimeout(2)
+            //       )
+            //     ),
 
-                // Then deploy intake after we're back on the floor
-                new ParallelCommandGroup(
-                  // new ClawInstantOpenCommand(),
-                  new IntakeArmRotateDumbCommand(0.6).withTimeout(2.0),
-                  new SequentialCommandGroup(
-                    new IntakeRollerRunCommand().withTimeout(2)
-                  )
-                ),
-
-                // Then stow intake
-                new IntakeStowConeCommandGroup()
-            ),
+            //     // Then stow intake
+            //     new IntakeStowConeCommandGroup()
+            // ),
             // Path movement
             new SequentialCommandGroup(
-                new WaitCommand(3.5),
+                new WaitCommand(0),
                 TrajectoryUtil.createTrajectoryPathCommand("grid1 1 cone balance", true)
         )
         );
